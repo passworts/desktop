6734f0626ba5751a9688f0ec15dc92d5b7503681
kievking <jinlongren100@gmail.com>
2020-06-02 18:41:46 +0200

    Fixed styling for login info

--- app/components/LoginInfo/Index.tsx
+++ app/components/LoginInfo/Index.tsx
@@ -3,7 +3,8 @@ import {
   TextField,
   Input,
   InputAdornment,
-  IconButton
+  IconButton,
+  makeStyles
 } from '@material-ui/core';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
@@ -13,12 +14,19 @@ type Props = {
   mode: string;
   dataRepInput: any;
 };
-
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    width: '90%'
+  }
+}));
 export default function LoginInfo({ dataRepInput, mode }: Props) {
   const dataRep = dataRepInput;
   const [name, setName] = useState(dataRep.name);
   const [note, setNote] = useState(dataRep.note);
   const [showPassword, setShowPassword] = useState(false);
+  const classes = useStyles();
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   };
@@ -32,6 +40,7 @@ export default function LoginInfo({ dataRepInput, mode }: Props) {
         label="Name"
         value={name}
         disabled={mode === 'view'}
+        className={classes.textField}
         onChange={(event: any) => {
           dataRep.name = event.target.value;
           setName(event.target.value);
@@ -48,6 +57,7 @@ export default function LoginInfo({ dataRepInput, mode }: Props) {
         label="Note"
         value={note}
         disabled={mode === 'view'}
+        className={classes.textField}
         onChange={(event: any) => {
           dataRep.note = event.target.value;
           setNote(event.target.value);
@@ -57,6 +67,7 @@ export default function LoginInfo({ dataRepInput, mode }: Props) {
         label="Username"
         defaultValue={dataRep.username}
         disabled={mode === 'view'}
+        className={classes.textField}
         onChange={(event: any) => {
           dataRep.username = event.target.value;
         }}
@@ -66,6 +77,7 @@ export default function LoginInfo({ dataRepInput, mode }: Props) {
         type={showPassword ? 'text' : 'password'}
         defaultValue={dataRep.password}
         disabled={mode === 'view'}
+        className={classes.textField}
         onChange={(event: any) => {
           dataRep.password = event.target.value;
         }}
@@ -85,6 +97,7 @@ export default function LoginInfo({ dataRepInput, mode }: Props) {
         label="Url"
         defaultValue={dataRep.url}
         disabled={mode === 'view'}
+        className={classes.textField}
         onChange={(event: any) => {
           dataRep.url = event.target.value;
         }}
