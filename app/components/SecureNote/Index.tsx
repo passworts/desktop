65c270dd7f3c430bb87cb4ef23e68ceb6628e022
kievking <jinlongren100@gmail.com>
2020-06-02 16:21:17 +0200

    Fixed styles for secure notes

--- app/components/SecureNote/Index.tsx
+++ app/components/SecureNote/Index.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
-import { TextField } from '@material-ui/core';
+import { TextField, makeStyles } from '@material-ui/core';
 import styles from './SecureNote.css';
 
 type Props = {
   mode: string;
   dataRepInput: any;
 };
-
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    width: '90%'
+  }
+}));
 export default function SecureNote({ dataRepInput, mode }: Props) {
   const dataRep = dataRepInput;
   const [name, setName] = useState(dataRep.name);
   const [note, setNote] = useState(dataRep.note);
+  const classes = useStyles();
   return (
     <div className={styles.container} data-tid="container">
       <TextField
         label="Name"
         value={name}
+        className={classes.textField}
         disabled={mode === 'view'}
         onChange={(event: any) => {
           dataRep.name = event.target.value;
@@ -32,6 +40,7 @@ export default function SecureNote({ dataRepInput, mode }: Props) {
       <TextField
         label="Note"
         value={note}
+        className={classes.textField}
         disabled={mode === 'view'}
         onChange={(event: any) => {
           dataRep.note = event.target.value;
@@ -39,9 +48,14 @@ export default function SecureNote({ dataRepInput, mode }: Props) {
         }}
       />
       <TextField
-        label="Content"
+        id="outlined-multiline-static"
+        label="Multiline"
+        className={classes.textField}
+        multiline
+        rows={10}
         defaultValue={dataRep.content}
         disabled={mode === 'view'}
+        variant="outlined"
         onChange={(event: any) => {
           dataRep.content = event.target.value;
         }}
